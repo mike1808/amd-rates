7cbf9a85d88fdb0bf4d2a797cbcf85d63578ad3e
mike1808 <arm.localhost@gmail.com>
2013-08-06 22:19:32 +0400

    added several bank option

--- js/options.js
+++ js/options.js
@@ -5,14 +5,28 @@ var selectedBanks = [];
 function loadBanks() {
     $.getJSON("/js/banks.json", function(json){
         self.banks = json.banks;
-        self.generateBanksList();
-        $(':checkbox').change(function(e) {
-            if ($(this).is(':checked'))
-                self.selectedBanks.push(parseInt($(this).attr('id')));
-            else
-                self.selectedBanks.remove(parseInt($(this).attr('id')));
 
-        })
+        if (localStorage['selected-banks'])
+            self.selectedBanks = JSON.parse(localStorage['selected-banks']);
+        else
+            self.selectedBanks = JSON.parse('[]');
+
+        self.generateBanksList();
+
+
+        $(':checkbox').change(function(e) {
+            if ($(this).is(':checked')) {
+                self.selectedBanks.push({
+                    name: banks[]
+                    index: parseInt($(this).attr('id'))
+                });
+            }
+            else {
+                self.selectedBanks.remove(parseInt($(this).attr('id')));
+            }
+
+            localStorage['selected-banks'] = JSON.stringify(selectedBanks);
+        });
     });
 }
 
@@ -21,15 +35,19 @@ function generateBanksList() {
         var checkbox = document.createElement('input');
         checkbox.setAttribute('type', 'checkbox');
         checkbox.setAttribute('id', banks[i].index);
+
+        if (selectedBanks.indexOf(banks[i].index) >= 0)
+            checkbox.setAttribute('checked', '');
+
         var label = document.createElement('label');
         label.setAttribute('for', banks[i].index);
         label.appendChild(checkbox);
         label.innerHTML = label.innerHTML + banks[i].name;
+
         document.getElementsByClassName('bank-list')[0].appendChild(label);
     }
 }
 
 $(function(){
     loadBanks();
-
 });
--- js/popup.js
+++ js/popup.js
@@ -8,33 +8,48 @@ function getRates() {
         dataType: 'xml'
     })
         .success(function(html){
-            var rates = $('.rb tr', html)[15].children;
+            var selectedBanks = JSON.parse(localStorage['selected-banks']);
+            var fragment = document.createDocumentFragment();
 
-            var tr = document.createElement('tr');
+            for(var i=0; i<selectedBanks.length; i++) {
+               var rates = $('.rb tr', html)[selectedBanks[i]].children;
+               var tr = document.createElement('tr');
 
-            self.currentRates.USD.buy = rates[5].innerText;
-            self.currentRates.USD.sell = rates[6].innerText;
-            self.currentRates.EUR.buy = rates[7].innerText;
-            self.currentRates.EUR.sell = rates[8].innerText;
-            self.currentRates.RUR.buy = rates[9].innerText;
-            self.currentRates.RUR.sell = rates[10].innerText;
+               self.currentRates[selectedBanks[i]] = {
+                   USD: {
+                       buy: rates[5].innerText,
+                        sell: rates[6].innerText
+                   },
+                   EUR: {
+                       buy: rates[7].innerText,
+                       sell: rates[8].innerText
+                   },
+                   RUR: {
+                       buy: rates[9].innerText,
+                       sell: rates[10].innerText
+                   },
+                   AMD: {
+                       buy: 1,
+                       sell: 1
+                   }
+               };
 
-            for (var i = 5; i <= 10; i++) {
-                var td = document.createElement('td');
-                td.appendChild(document.createTextNode(rates[i].innerText));
-                tr.appendChild(td);
-            }
+               for (var j = 5; j <= 10; j++) {
+                   var td = document.createElement('td');
+                   td.appendChild(document.createTextNode(rates[j].innerText));
+                   tr.appendChild(td);
+               }
 
-            $('tbody').append(tr);
+               fragment.appendChild(tr);
+
+           }
+
+           $('tbody').append(fragment);
+
         });
 }
 
-var currentRates = {
-    USD: { sell: 1, buy: 1},
-    RUR: { sell: 1, buy: 1},
-    EUR: { sell: 1, buy: 1},
-    AMD: { sell: 1, buy: 1}
-}
+var currentRates = {};
 
 function setCurrency(e) {
     var currentElement = $(this);
@@ -49,12 +64,13 @@ function calculateCurrency(e) {
     var input = $(this);
     var inputType = input.attr('id');
     var reversedType = inputType == 'in' ? 'out' : 'in';
+    var expressionRegExp = /^[0-9\*\+\-\/\(\)\.\s]+$/;
 
     var ratio = currentRates[calcCurrencies[inputType]].sell /
         currentRates[calcCurrencies[reversedType]].sell;
     var value = 0;
-    if (!isNaN(input.val()))
-        value = parseInt(input.val()) * ratio;
+    if (expressionRegExp.test(input.val()))
+        value = parseInt(eval(input.val())) * ratio;
 
     $('#' + reversedType).val(value.toFixed(2));
 }
