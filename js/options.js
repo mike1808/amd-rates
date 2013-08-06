c462181cf41696553c21225ea4c8b3342e04b87c
Michael Manukyan <arm.localhost@gmail.com>
2013-08-06 18:47:04 +0400

    added options page

--- js/options.js
+++ js/options.js
@@ -1,13 +1,27 @@
 var self = this;
-var banks = {};
+var banks = [];
 var selectedBanks = [];
 
 function loadBanks() {
-    $.getJSON("banks.json", function(json){
-        self.banks = json;
+    $.getJSON("/js/banks.json", function(json){
+        self.banks = json.banks;
+        self.generateBanksList();
     });
 }
 
 function generateBanksList() {
-    for (var i = 0; i < banks.length)
+    for (var i = 0; i < banks.length; i++) {
+        var checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox');
+        checkbox.setAttribute('id', banks[i].index);
+        var label = document.createElement('label');
+        label.setAttribute('for', banks[i].index);
+        label.appendChild(checkbox);
+        label.innerHTML = label.innerHTML + banks[i].name;
+        document.body.appendChild(label);
+    }
 }
+
+$(function(){
+    loadBanks();
+});
